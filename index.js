216c6c35c56b71e1bc58060b78ac3023b6a01b18
Tyler Jowers <Jowers.tc@protonmail.com>
2019-03-12 22:33:40 -0500

    working on getAncestors

---
+++ index.js
@@ -0,0 +1,20 @@
+export const toAbs = function(e){
+  const p = pos(e)
+  e.style.position = 'absolute'
+  e.style.top = p.y + 'px'
+  e.style.bottom = p.x + 'px'
+  return pos(e)
+}
+
+export const grab = function(id){
+	return document.getElementById(id);
+}
+
+export const pos = function(e){
+  const p = e.getBoundingClientRect()
+  return {x: p.left, y: p.top}
+}
+
+export const getAncestors = function(e){
+
+}
